569a1e4c21f5d6899a85c1ba708d7a2d456d15ed
Tinko <TinkoLiu@users.noreply.github.com>
2020-03-25 16:38:04 +0800

    build: update eslint and packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,32 +5,20 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/strongly-recommended',
-    '@vue/standard'
+    '@vue/standard',
+    'standard'
   ],
   rules: {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'generator-star-spacing': 'off',
     'no-mixed-operators': 0,
-    'vue/max-attributes-per-line': [
-      2,
-      {
-        'singleline': 5,
-        'multiline': {
-          'max': 1,
-          'allowFirstLine': false
-        }
-      }
-    ],
     'vue/attribute-hyphenation': 0,
-    'vue/html-self-closing': 0,
     'vue/component-name-in-template-casing': 0,
-    'vue/html-closing-bracket-spacing': 0,
     'vue/singleline-html-element-content-newline': 0,
     'vue/no-unused-components': 0,
     'vue/multiline-html-element-content-newline': 0,
     'vue/no-use-v-if-with-v-for': 0,
-    'vue/html-closing-bracket-newline': 0,
     'vue/no-parsing-error': 0,
     'no-tabs': 0,
     'quotes': [
